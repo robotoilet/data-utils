d4dd2b4f9daf12ebac997ae88d50f195aca41954
Alecs Geuder <al.geuder@gmail.com>
2014-11-19 16:52:43 +0000

    don't log into non-existing dir

--- logger.js
+++ logger.js
@@ -6,7 +6,7 @@ var logger = new winston.Logger({
     transports: [
         new winston.transports.File({
             level: 'info',
-            filename: './logs/all-logs.log',
+            filename: './all-logs.log',
             handleExceptions: true,
             json: true,
             maxsize: 5242880, //5MB
